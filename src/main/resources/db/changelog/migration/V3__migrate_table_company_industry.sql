76c7bfc50bc4419fb5af88db42350a2207c456ed
Victor Revenco <victor.revenco@isa.utm.md>
2025-07-03 14:03:22 +0300

    Add timestamps for inserted at and updated at

--- src/main/resources/db/changelog/migration/V3__migrate_table_company_industry.sql
+++ src/main/resources/db/changelog/migration/V3__migrate_table_company_industry.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS company_industry (
     id         BIGSERIAL PRIMARY KEY,
     company_id BIGINT REFERENCES company(id),
     industry_id BIGINT REFERENCES industry(id),
+    inserted_at TIMESTAMP,
+    updated_at TIMESTAMP,
     CONSTRAINT uq_company_product UNIQUE (company_id, industry_id)
 );
--- src/main/resources/db/changelog/migration/V5__migrate_table_tax_info.sql
+++ src/main/resources/db/changelog/migration/V5__migrate_table_tax_info.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS tax_info (
     tax_registration_number TEXT UNIQUE NOT NULL,
     date_of_registration DATE,
     tax_payer_type TEXT,
+    inserted_at TIMESTAMP,
+    updated_at TIMESTAMP,
     tax_authority_id BIGINT NOT NULL REFERENCES tax_authority(id),
     company_id BIGINT NOT NULL REFERENCES company(id)
 );
